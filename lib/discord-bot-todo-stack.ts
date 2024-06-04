f55d766e285e427abaeedd654129501408a44235
dannyzou18 <dzou3814@gmail.com>
2024-06-03 23:34:09 -0400

    dynamoDB setup, addtask command works succesfully

--- lib/discord-bot-todo-stack.ts
+++ lib/discord-bot-todo-stack.ts
@@ -2,10 +2,17 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 
 export class DiscordBotTodoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const table = new dynamodb.Table(this, 'TasksTable2', {
+      partitionKey: { name: 'taskId', type: dynamodb.AttributeType.NUMBER },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+    });
 
     const dockerFunction = new lambda.DockerImageFunction(
       this,
@@ -17,9 +24,12 @@ export class DiscordBotTodoStack extends cdk.Stack {
         architecture: lambda.Architecture.X86_64,
         environment: {
           DISCORD_PUBLIC_KEY: "1fd00a1eedaa0e600ef43e89c2678800127ad6841d7bbf43418e0fda0af90314",
+          TASKS_TABLE_NAME: table.tableName,
         },
       }
     );
+
+    table.grantReadWriteData(dockerFunction);
 
     const functionUrl = dockerFunction.addFunctionUrl({
       authType: lambda.FunctionUrlAuthType.NONE,
